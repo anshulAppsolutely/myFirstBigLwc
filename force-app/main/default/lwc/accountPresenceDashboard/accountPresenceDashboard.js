f0798adef9ccb381044c0738c36b8296a5249246
Dan Nicholson <dan.nicholson@appsolutely.nl>
2019-02-11 15:41:20 +0000

    Completed example bubble chart. Resizes to available width

--- force-app/main/default/lwc/accountPresenceDashboard/accountPresenceDashboard.js
+++ force-app/main/default/lwc/accountPresenceDashboard/accountPresenceDashboard.js
@@ -6,8 +6,6 @@ import DATA from './data';
 
 export default class AccountPresenceDashboard extends LightningElement {
 
-    svgWidth = 600;
-    svgHeight = 600;
     d3Initialized = false;
 
     /**
@@ -26,7 +24,8 @@ export default class AccountPresenceDashboard extends LightningElement {
             loadStyle(this, D3 + '/style.css')
         ]).then(() => {
             //initialize the graph
-            this.initializeD3();
+            this.bubbleChart();
+
         }).catch(error => {
             //show error if problem in loading d3
             this.dispatchEvent(
@@ -39,10 +38,106 @@ export default class AccountPresenceDashboard extends LightningElement {
         });
     }
 
+    bubbleChart() {
+
+        var dataset = {
+            "children": [{"Name":"Olives","Count":4319},
+                {"Name":"Tea","Count":4159},
+                {"Name":"Mashed Potatoes","Count":2583},
+                {"Name":"Boiled Potatoes","Count":2074},
+                {"Name":"Milk","Count":1894},
+                {"Name":"Chicken Salad","Count":1809},
+                {"Name":"Vanilla Ice Cream","Count":1713},
+                {"Name":"Cocoa","Count":1636},
+                {"Name":"Lettuce Salad","Count":1566},
+                {"Name":"Lobster Salad","Count":1511},
+                {"Name":"Chocolate","Count":1489},
+                {"Name":"Apple Pie","Count":1487},
+                {"Name":"Orange Juice","Count":1423},
+                {"Name":"American Cheese","Count":1372},
+                {"Name":"Green Peas","Count":1341},
+                {"Name":"Assorted Cakes","Count":1331},
+                {"Name":"French Fried Potatoes","Count":1328},
+                {"Name":"Potato Salad","Count":1306},
+                {"Name":"Baked Potatoes","Count":1293},
+                {"Name":"Roquefort","Count":1273},
+                {"Name":"Stewed Prunes","Count":1268}]
+        };
+        
+        var width = d3.select(this.template.querySelector('div.d3')).node().getBoundingClientRect().width; // Resize based on available width
+        var height = width * 0.6; // Fixed aspect
+        
+        var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+        var bubble = d3.pack(dataset)
+            .size([width, height])
+            .padding(1.5);
+
+        var svg = d3.select(this.template.querySelector('div.d3')).append('svg')
+            .attr("width", width)
+            .attr("height", height)
+            .attr("class", "bubble");
+
+        var nodes = d3.hierarchy(dataset)
+            .sum(function(d) { return d.Count; });
+
+        var node = svg.selectAll(".node")
+            .data(bubble(nodes).descendants())
+            .enter()
+            .filter(function(d){
+                return  !d.children
+            })
+            .append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+
+        node.append("title")
+            .text(function(d) {
+                return d.Name + ": " + d.Count;
+            });
+
+        node.append("circle")
+            .attr("r", function(d) {
+                return d.r;
+            })
+            .style("fill", function(d,i) {
+                return color(0); // pass i to change colour on each bubble
+            });
+
+        node.append("text")
+            .attr("dy", ".2em")
+            .style("text-anchor", "middle")
+            .text(function(d) {
+                return d.data.Name.substring(0, d.r / 3);
+            })
+            .attr("font-family", "sans-serif")
+            .attr("font-size", function(d){
+                return d.r/5;
+            })
+            .attr("fill", "white");
+
+        node.append("text")
+            .attr("dy", "1.3em")
+            .style("text-anchor", "middle")
+            .text(function(d) {
+                return d.data.Count;
+            })
+            .attr("font-family",  "Gill Sans", "Gill Sans MT")
+            .attr("font-size", function(d){
+                return d.r/5;
+            })
+            .attr("fill", "white");
+
+        d3.select(self.frameElement)
+            .style("height", height + "px");
+    }
+
     /**
-     * method used to initialize graph
+     * method used to initialize graph (old chart - to delete)
      */
-    initializeD3() {
+    /*initializeD3() {
 
         //get the reference container through svg
         const svg = d3.select(this.template.querySelector('svg.d3'));
@@ -132,5 +227,5 @@ export default class AccountPresenceDashboard extends LightningElement {
             d.fx = null;
             d.fy = null;
         }
-    }
+    } */
 }
