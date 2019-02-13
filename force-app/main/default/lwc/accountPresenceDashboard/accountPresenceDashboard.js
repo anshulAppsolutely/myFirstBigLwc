f4c774c5481af1cbe59c1dc35cc16cf6fcecadd7
Dan Nicholson <dan.nicholson@appsolutely.nl>
2019-02-13 15:57:00 +0000

    Using dummy data, not filtering accounts

--- force-app/main/default/lwc/accountPresenceDashboard/accountPresenceDashboard.js
+++ force-app/main/default/lwc/accountPresenceDashboard/accountPresenceDashboard.js
@@ -1,12 +1,47 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import D3 from '@salesforce/resourceUrl/d3';
-import DATA from './data';
+// import DATA from './data';
+// import Selector from '@salesforce/schema/EmailDomainKey.Selector';
+// import { getFieldValue } from 'lightning/uiRecordApi';
+// import UserPreferencesShowTitleToExternalUsers from '@salesforce/schema/UserChangeEvent.UserPreferencesShowTitleToExternalUsers';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class AccountPresenceDashboard extends LightningElement {
+import DATASET from './account-presence-response'; 
+import getAccountList from '@salesforce/apex/OwlinEntitiesManagementController.getAccountList';
+
+
+export default class AccountPresenceDashboard extends NavigationMixin(LightningElement) {
 
     d3Initialized = false;
+    scriptsImported = false;
+
+    @api
+    bubbleColor;
+
+    @api
+    textColor;
+
+    @api
+    statKey = 'delta:month';
+    
+    @track accounts;
+
+    /** Get accounts from Apex */
+    @wire(getAccountList)
+    wiredAccounts({ error, data }) {
+        if (data) {
+            this.accounts = data;
+            // this.outputProxy(data);
+            //initialize the graph if scripts imported
+            if (this.scriptsImported) 
+                this.bubbleChart(this);
+        } else if (error) {
+            this.errorToast(error.body.message);
+            this.accounts = undefined;
+        }
+    }
 
     /**
      * ensures that the page loads and renders
@@ -23,64 +58,40 @@ export default class AccountPresenceDashboard extends LightningElement {
             loadScript(this, D3 + '/d3.V5.min.js'),
             loadStyle(this, D3 + '/style.css')
         ]).then(() => {
-            //initialize the graph
-            this.bubbleChart();
+            //initialize the graph if accounts created
+            if (this.accounts)
+                this.bubbleChart(this);
 
         }).catch(error => {
             //show error if problem in loading d3
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Error loading D3',
-                    message: error.message,
-                    variant: 'error'
-                })
-            );
+            this.errorToast(error.message);
         });
     }
 
-    bubbleChart() {
+    bubbleChart(c) {
+        // bind container object for reference
+        var container = c;
 
-        var dataset = {
-            "children": [{"Name":"Olives","Count":4319},
-                {"Name":"Tea","Count":4159},
-                {"Name":"Mashed Potatoes","Count":2583},
-                {"Name":"Boiled Potatoes","Count":2074},
-                {"Name":"Milk","Count":1894},
-                {"Name":"Chicken Salad","Count":1809},
-                {"Name":"Vanilla Ice Cream","Count":1713},
-                {"Name":"Cocoa","Count":1636},
-                {"Name":"Lettuce Salad","Count":1566},
-                {"Name":"Lobster Salad","Count":1511},
-                {"Name":"Chocolate","Count":1489},
-                {"Name":"Apple Pie","Count":1487},
-                {"Name":"Orange Juice","Count":1423},
-                {"Name":"American Cheese","Count":1372},
-                {"Name":"Green Peas","Count":1341},
-                {"Name":"Assorted Cakes","Count":1331},
-                {"Name":"French Fried Potatoes","Count":1328},
-                {"Name":"Potato Salad","Count":1306},
-                {"Name":"Baked Potatoes","Count":1293},
-                {"Name":"Roquefort","Count":1273},
-                {"Name":"Stewed Prunes","Count":1268}]
-        };
-        
         var width = d3.select(this.template.querySelector('div.d3')).node().getBoundingClientRect().width; // Resize based on available width
         var height = width * 0.6; // Fixed aspect
-        
-        var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-        var bubble = d3.pack(dataset)
-            .size([width, height])
-            .padding(1.5);
+        //@todo replace DATASET with live response from Owlin
+        var bubble = d3.pack(DATASET)
+            .size([width, height]);
 
-        var svg = d3.select(this.template.querySelector('div.d3')).append('svg')
+        // Add SVG to render bubble chart
+        var svg = d3.select(this.template.querySelector('div.d3'))
+            .append('svg')
             .attr("width", width)
             .attr("height", height)
-            .attr("class", "bubble");
 
-        var nodes = d3.hierarchy(dataset)
-            .sum(function(d) { return d.Count; });
+        // Build nodes
+        var nodes = d3.hierarchy(DATASET)
+            .sum(function(d) {
+                return getValue(d);
+            });
 
+        // For each node, populate with bubble
         var node = svg.selectAll(".node")
             .data(bubble(nodes).descendants())
             .enter()
@@ -92,46 +103,90 @@ export default class AccountPresenceDashboard extends LightningElement {
             .attr("transform", function(d) {
                 return "translate(" + d.x + "," + d.y + ")";
             });
-
+        
         node.append("title")
             .text(function(d) {
-                return d.Name + ": " + d.Count;
+                return d.data.title;
             });
 
         node.append("circle")
             .attr("r", function(d) {
                 return d.r;
             })
-            .style("fill", function(d,i) {
-                return color(0); // pass i to change colour on each bubble
-            });
+            .style("fill", this.bubbleColor);
 
         node.append("text")
             .attr("dy", ".2em")
             .style("text-anchor", "middle")
             .text(function(d) {
-                return d.data.Name.substring(0, d.r / 3);
+                return getTitle(d.data);
             })
             .attr("font-family", "sans-serif")
             .attr("font-size", function(d){
                 return d.r/5;
             })
-            .attr("fill", "white");
+            .attr("fill", this.textColor)
+        
+        // Handle click on node
+        node.on('click', function (d) {container.navigateToAccount(d.data.key)});
 
-        node.append("text")
-            .attr("dy", "1.3em")
-            .style("text-anchor", "middle")
-            .text(function(d) {
-                return d.data.Count;
-            })
-            .attr("font-family",  "Gill Sans", "Gill Sans MT")
-            .attr("font-size", function(d){
-                return d.r/5;
-            })
-            .attr("fill", "white");
+        // Get appropriate value for account (bubble size)
+        function getValue(d) {
+            var statKey = 'delta:month';
+            return d.stats && d.stats[statKey] !== undefined ? d.stats[statKey].value : 0;
+        }
 
-        d3.select(self.frameElement)
-            .style("height", height + "px");
+        // Get title for account
+        function getTitle(d){
+            // d.data.title.substring(0, d.r / 3)
+            return d.title.substring(0,10);
+        }
+    }
+
+    // Find matching account by entity ID and navigate.
+    navigateToAccount(entityId) {
+        var i;
+        for (i = 0; i < this.accounts.length; i++) {
+            if (entityId === this.accounts[i].Owlin_Entity_Id__c) {
+                this[NavigationMixin.Navigate]({
+                    type: "standard__recordPage",
+                    attributes: {
+                        recordId: this.accounts[i].Id,
+                        objectApiName: "Account",
+                        actionName: "view"
+                    }
+                });
+                return;
+            }
+        }
+        this.errorToast('Could not find matching account.');
+    }
+
+    /** 
+     * Show error toast with message 
+     * @todo labels
+     */ 
+    errorToast(message) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Error',
+                message: message,
+                variant: 'error'
+            })
+        );
+    }
+
+    /** this is just a helper method for development. */
+    outputProxy(record) {
+        var obj = {};
+        for(var propt in record) {
+            obj[propt] = record[propt];
+            if (typeof(record[propt]) == 'object') {
+                obj[propt] = this.outputProxy(record[propt]);
+            }
+        }
+        console.log(obj);
+        return obj;
     }
 
     /**
