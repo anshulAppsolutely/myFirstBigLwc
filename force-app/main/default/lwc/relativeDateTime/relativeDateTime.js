0de43f550e7beb6fe772b2b96c8ee5df3c3ddca6
Anshul Agrawal <anshul.agrawal@appsolutely.nl>
2019-02-26 22:09:53 +0100

    modifications to topnews!

--- force-app/main/default/lwc/relativeDateTime/relativeDateTime.js
+++ force-app/main/default/lwc/relativeDateTime/relativeDateTime.js
@@ -13,10 +13,12 @@ export default class RelativeDateTime extends LightningElement {
 
     set epochDate(value) {
         console.log('in relative time  >>'+value);
-        var utcSeconds = value;
-        var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
-        d.setUTCSeconds(utcSeconds);
-        this._epochDate = d;
-        console.log('in relative time  2>>'+this._epochDate );
+        if(value!=null) {
+            var utcSeconds = value;
+            var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+            d.setUTCSeconds(utcSeconds);
+            this._epochDate = d;
+            console.log('in relative time  2>>' + this._epochDate);
+        }
     }
 }
