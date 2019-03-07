a7c7df1b405109775bfb6b14592029581ece6451
Anshul Agrawal <anshul.agrawal@appsolutely.nl>
2019-03-07 15:24:08 +0100

    test class and relativetimelinechecks

--- force-app/main/default/lwc/topNews/topNews.js
+++ force-app/main/default/lwc/topNews/topNews.js
@@ -1,5 +1,7 @@
 import { LightningElement, track , api, wire} from 'lwc';
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import getTopNewsData from '@salesforce/apex/OwlinEntitiesManagementController.getTopNewsData';
+import MomentAsset from '@salesforce/resourceUrl/moment';
 // Labels
 import Error_Title from '@salesforce/label/c.Error_Title';
 import Error_NoData from '@salesforce/label/c.Error_NoData';
@@ -39,6 +41,21 @@ export default class TopNews extends LightningElement {
         Error_Title,
         Error_NoData
     };
+
+    /**
+     * ensures that the page loads and renders
+     * the container before the graph is created
+     */
+    renderedCallback() {
+        //load the scripts
+        Promise.all([
+            loadScript(this, MomentAsset + '/moment-with-locales.min.js'),
+        ]).then(() => {
+            //do nothing use this in relative time
+        }).catch(error => {
+            //this impact only a small part relative time. topnews component should be rendered anyways without relative time
+        });
+    }
 
     get headerColorToDisplay () {return 'color:'+this.headerColor}
 
